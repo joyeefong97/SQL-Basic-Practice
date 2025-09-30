5fefbeb755bdb1bbe0be3af2ca580e3be26e02e4
joyeefong97 <joyeefong97@gmail.com>
2025-09-30 16:39:18 +0800

    Add files via upload
    SELECT Statement Practice

---
+++ SQL SELECT Basic Practice.sql
@@ -0,0 +1,19 @@
+# Find title of each film 
+SELECT Title
+FROM movies;
+
+# Find director of each film 
+SELECT Director
+FROM movies;
+
+# Find title and director of each film 
+SELECT Title, Director
+FROM movies;
+
+#  Find title and year of each film
+SELECT Title, Year
+FROM movies;
+
+#  Find all information of each film 
+SELECT *
+FROM movies;
